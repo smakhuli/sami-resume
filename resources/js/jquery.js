9eaa939be10b0946519ef379733dd91bf931982f
Sami Makhuli <sami.makhuli@ibethel.org>
2019-02-24 06:01:55 -0800

    added navigation for device/browser sizes of 767 or less (iPhones)

--- resources/js/jquery.js
+++ resources/js/jquery.js
@@ -47,4 +47,21 @@ $(document).ready(function() {
           }
         }
       });
+    
+    /* Mobile Navigation */
+    $('.js--nav-icon').click(function() {
+        var nav = $('.js--main-nav')
+        var icon = $('.js--nav-icon i')
+        
+        nav.slideToggle(200);
+        
+        if (icon.hasClass('ion-navicon-round')) {
+            icon.removeClass('ion-navicon-round')
+            icon.addClass('ion-close-round')
+        } else {
+            icon.removeClass('ion-close-round')
+            icon.addClass('ion-navicon-round')
+        }
+    })
+
 });
